9d99683de934ac380cfe46c064247842b6758e56
b.vilishchuk@edu.internetdevels.com <b.vilishchuk@edu.internetdevels.com>
2020-07-23 18:28:08 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://quaded.com/data/android-logo-150x150.png" />
+    </header>
+  );
+};
+export default Header;
